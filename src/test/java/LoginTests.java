2d17d9075145a69ac95dcd79349536a46402df72
czar-testpro <czar@testpro.io>
2025-08-05 22:14:10 +0800

    Updated TODO comment

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -17,7 +17,8 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        // TODO: Something's off with this URL. See if you can fix it before we start automation!
+        // TODO (for students): Review the configuration as part of HW15
+
         String url = "httpps://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
